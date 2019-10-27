0ba0e921b0b902e8722ff2ad807b7c52ab1f9822
andreas <androgles1994@gmail.com>
2019-10-27 10:02:07 +0100

    -

--- api/db/db.js
+++ api/db/db.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose')
 
-console.log("db url", process.env.MONGODB_URL)
+console.log("db url", "mongodb://heroku_kv70jnvj:g707bj1hhhctvam4faa80at0b6@ds239858.mlab.com:39858/heroku_kv70jnvj")
 
-mongoose.connect("mongodb://heroku_kv70jnvj:g707bj1hhhctvam4faa80at0b6@ds239858.mlab.com:39858/heroku_kv70jnvj", {
+mongoose.connect('mongodb://heroku_kv70jnvj:g707bj1hhhctvam4faa80at0b6@ds239858.mlab.com:39858/heroku_kv70jnvj', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true 
 })
 
 mongoose.connection.on('connected', () => {
-    console.log(`Mongoose connected to ${process.env.MONGODB_URL}`);
+    console.log(`Mongoose connected to mongodb://heroku_kv70jnvj:g707bj1hhhctvam4faa80at0b6@ds239858.mlab.com:39858/heroku_kv70jnvj`);
 });
 mongoose.connection.on('error', err => {
     console.log('Mongoose connection error:', err);
